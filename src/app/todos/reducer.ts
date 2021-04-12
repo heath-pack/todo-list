8d397cb22062e34e0bfc11ee32117b5ff4196625
Heath Pack <hpack230@gmail.com>
2021-04-11 21:40:15 -0400

    added navList, ability to add a new list of todos, actions types, etc...

--- src/app/todos/reducer.ts
+++ src/app/todos/reducer.ts
@@ -1,31 +1,90 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { v4 as uniqueId } from 'uuid';
 
-import { addTodo, completeTodo, deleteTodo } from './actions';
+import {
+  addTodo,
+  completeTodo,
+  deleteTodo,
+  addTodoToList,
+  addTodoList,
+  deleteList
+} from './actions';
 
 export type Todo = {
   id: string;
   value: string;
   completed: boolean;
+  listId: string;
 };
 
-type TodoState = Record<Todo['id'], Todo>;
+type Todos = Record<Todo['id'], Todo>;
 
-const initialState: TodoState = {};
+type TodoList = { listId: Todo['listId']; name: string };
+
+type TodoLists = Record<TodoList['listId'], TodoList>;
+
+type TodosState = {
+  todos: Todos;
+  todoLists: TodoLists;
+  selectedList: TodoList['listId'];
+};
+
+const initialState: TodosState = {
+  todoLists: {},
+  todos: {},
+  selectedList: ''
+};
 
 export const todosReducer = createReducer(initialState, (builder) => {
   builder.addCase(addTodo, (state, action) => {
+    const { listId, value } = action.payload;
     const id = uniqueId();
-    state[id] = { value: action.payload, id, completed: false };
+
+    state.todos[id] = {
+      value,
+      id,
+      completed: false,
+      listId
+    };
   });
 
   builder.addCase(deleteTodo, (state, action) => {
-    delete state[action.payload];
+    delete state.todos[action.payload];
   });
 
   builder.addCase(completeTodo, (state, action) => {
-    const todo = state[action.payload];
-    state[action.payload] = { ...todo, completed: !todo.completed };
+    const todo = state.todos[action.payload];
+    state.todos[action.payload] = { ...todo, completed: !todo.completed };
+  });
+
+  builder.addCase(addTodoToList, (state, action) => {
+    const todoId = action.payload;
+    state.todos[todoId] = {
+      ...state.todos[todoId],
+      listId: state.selectedList
+    };
+  });
+
+  builder.addCase(addTodoList, (state, action) => {
+    const name = action.payload;
+    const listId = uniqueId();
+
+    state.todoLists[listId] = { listId, name };
+  });
+
+  builder.addCase(deleteList, (state, action) => {
+    const id = action.payload;
+
+    delete state.todoLists[id];
+
+    const matchingTodos = Object.values(state.todos).filter(
+      (todo) => todo.listId !== id
+    );
+
+    state.todos = matchingTodos.reduce(
+      (object, todo) => ({ ...object, [todo.id]: todo }),
+      {}
+    );
   });
 
   builder.addDefaultCase((state, action) => {});
--- src/app/todos/todoList/TodoList.tsx
+++ src/app/todos/todoList/TodoList.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { getTodos } from '../selectors';
+import { getSelectedTodos } from '../selectors';
 import { TodoItem } from '../todoItem/Todo';
 
 export const TodoList = () => {
-  const todos = useSelector(getTodos);
+  const todos = useSelector(getSelectedTodos);
 
   return (
     <div>
