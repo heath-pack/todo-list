d594d41babca6c6695662b5ce9e36aa5f73ac0b6
Heath Pack <hpack230@gmail.com>
2021-04-18 08:10:33 -0400

    updated formating of components in the layout

--- src/app/todos/todosNavList/NavList.tsx
+++ src/app/todos/todosNavList/NavList.tsx
@@ -3,15 +3,15 @@ import { useSelector } from 'react-redux';
 
 import { getTodoLists } from '../selectors';
 
+import './navList.scss';
+
 export const NavList = () => {
   const lists = useSelector(getTodoLists);
 
   return (
-    <ul>
+    <ul className="navlist">
       {lists.map(({ listId, name }) => (
-        <li key={listId}>
-          <div>{name}</div>
-        </li>
+        <li key={listId}>{name}</li>
       ))}
     </ul>
   );
